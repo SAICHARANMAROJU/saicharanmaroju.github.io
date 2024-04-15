a51569dc4145edffb0bfbd5a2589834e8346a074
SAI CHARAN MAROJU <saicharanmaroju23@gmail.com>
2024-04-15 18:16:25 +0530

    commit

--- script.js
+++ script.js
@@ -1,16 +1,62 @@
-function myMenuFunction(){
+function myMenuFunction() {
     var menuBth = document.getElementById("myNavMenu");
-    if (menuBth.className ==="nav-menu"){
+    if (menuBth.className === "nav-menu") {
         menuBth.className += "responsive";
-    }
-    else{
+    } else {
         menuBth.className = "nav-menu";
-        
     }
-}
-
-const body = document.querySelector("body"),
-    toogleSwitch = document.getElementById("toggle-switch");
-toogleSwitch.addEventListener("click", ()=>{
+  }
+  
+  const body = document.querySelector("body"),
+    toggleSwitch = document.getElementById("toggle-switch");
+  toggleSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
-})
+  });
+  
+  
+  
+  /* Typing Effect */
+  document.addEventListener('DOMContentLoaded', function() {
+    const textElement = document.querySelector('.typedText');
+    const textArray = ["Designer","|","Coder","|","Developer","|"];
+    let textIndex = 0;
+    let charIndex = 0;
+  
+    function type() {
+        if (charIndex < textArray[textIndex].length) {
+            textElement.textContent += textArray[textIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 100); 
+        } else {
+            setTimeout(erase, 1000);
+        }
+    }
+  
+    function erase() {
+        if (charIndex > 0) {
+            textElement.textContent = textArray[textIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, 50); 
+        } else {
+            textIndex++;
+            if (textIndex >= textArray.length) {
+                textIndex = 0;
+            }
+            setTimeout(type, 500); 
+        }
+    }
+  
+    type();
+  });
+  
+  // Smooth scrolling effect for internal links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+  
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
+  
